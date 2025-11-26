c4e2ccc52dfe66692cc148ad729999da1b8fce6c
Fedex95 <fedex95@outlook.es>
2025-11-26 00:36:39 -0500

    first steps for home, navbar and layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Layout from './components/home/Layout';
 import Home from './components/home/Home';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Verify from './components/auth/Verify';
 
 function App() {
-
   return (
     <Router>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify" element={<Verify />} />
         </Routes>
       </Layout>
     </Router>
--- src/test/Home.test.jsx
+++ src/test/Home.test.jsx
@@ -1,16 +1,46 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import Home from '../components/home/Home';
 
 describe('Home Component', () => {
-  test('renders welcome message', () => {
-    render(<Home />);
-    expect(screen.getByText('Bienvenido a Library Master')).toBeInTheDocument();
-  });
-
   test('renders list of libros', () => {
-    render(<Home />);
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
     expect(screen.getByText('Libro 1')).toBeInTheDocument();
     expect(screen.getByText('Libro 2')).toBeInTheDocument();
+  });
+
+  test('renders libro details', () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    expect(screen.getByText('Descripción 1')).toBeInTheDocument();
+    expect(screen.getByText('Autor: Autor 1')).toBeInTheDocument();
+  });
+
+  test('renders ver detalles button for each libro', () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    const buttons = screen.getAllByText('Ver detalles');
+    expect(buttons).toHaveLength(2);
+  });
+
+  test('renders libro images', () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    const images = screen.getAllByAltText(/Libro/);
+    expect(images).toHaveLength(2);
   });
 });

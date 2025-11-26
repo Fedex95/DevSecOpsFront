62efaf5f41b105c5f08778208512cfeb7622aed3
Fedex95 <fedex95@outlook.es>
2025-11-25 23:46:35 -0500

    Merge pull request 1 from Frbles/secfront

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Layout from './components/Layout';
-import Home from './components/Home';
+import Layout from './components/home/Layout';
+import Home from './components/home/Home';
 
 function App() {
 
